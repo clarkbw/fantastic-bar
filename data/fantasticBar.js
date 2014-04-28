5b321106d21f60e95b0a640873fdd70783259647
Erik Vold <evold@mozilla.com>
2014-04-27 19:46:23 -0700

    previews are associated with the results now

--- data/fantasticBar.js
+++ data/fantasticBar.js
@@ -9,62 +9,16 @@ self.port.on('size', function({ height }) {
   let output = $('results-list').setAttribute('style', 'max-height: ' + height + 'px');
 });
 
-self.port.on('show-preview', function({ url }) {
-  let frames = document.getElementsByTagName("iframe");
-  for (let i = frames.length - 1; i >= 0; i--) {
-    let frame = frames[i];
-    if (frame.src == url) {
-      frame.removeAttribute('style');
-    }
-  }
-});
-
 self.port.on('query', function({ query, results }) {
   let output = $('results-list');
-  let preview_out = $('preview');
-  // clear old results
   output.innerHTML = '';
-  preview_out.innerHTML = '';
 
-  // clear the preview
-  // preview.innerHTML = '';
-  /*
-  let count = 0;
-  let newPrewviewOutput = '';
-  previews.forEach(function({ url }) {
-    newPrewviewOutput += '<iframe id="frame-' + count++ + '" style="visibility: hidden;" data-url="'+url+'" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="' + url + '"' + (count == previews.length ? ' class="slide"' : '') + '></iframe>';
-  });
-  if (newPrewviewOutput.length) {
-    $('results').removeAttribute('style');
-    preview_out.removeAttribute('style');
-    preview_out.innerHTML = newPrewviewOutput;
-  }
-  else {
-    $('results').setAttribute('style', 'width: 100%; max-width: 100%;');
-    preview_out.setAttribute('style', 'display: none;');
-  }
-  */
-
-  let frames = document.getElementsByTagName( "iframe" );
-
-/*
-  let displayed = count - 1;
-  clearInterval(lastInterval);
-  lastInterval = setInterval(function() {
-    let oldD = frames[displayed];
-
-    oldD && oldD.setAttribute('class', 'slide-off');
-
-    let newI = (--displayed < 0 ? (displayed = previews.length - 1) : displayed);
-    let newD = frames[newI];
-
-    newD && newD.setAttribute('class', 'slide');
-  }, 10000);
-*/
-
-  results.forEach(function(result) {
+  results.forEach(result => {
     let li = document.createElement('li');
     li.innerHTML = '<span class="link-title">' + result.title + '</span><span class="link">' + result.url + '</span>';
+    li.addEventListener('mouseover', function(event) {
+      loadPreview(result);
+    }, false);
     li.addEventListener('click', function() {
       self.port.emit('goto', result);
     }, false);
@@ -72,7 +26,15 @@ self.port.on('query', function({ query, results }) {
   })
 });
 
+function loadPreview({ url }) {
+  let iframe = $('preview_iframe');
+  if (iframe.getAttribute('data-url') == url) {
+    return;
+  }
+  iframe.src = url;
+  iframe.setAttribute('data-url', url);
+}
+
 function shouldStop() {
 
 }
-
